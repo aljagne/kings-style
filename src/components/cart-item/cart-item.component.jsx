9412ed0a6895149b259b78cef14ce7faf9f28ce8
Alieu  Jagne <ajagne01@gmail.com>
2020-03-23 13:26:11 +0100

    changed the json package and project name

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -9,7 +9,7 @@ const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
         <div className='item-details'>
             <span className='name'>{name}</span>
             <span className='price'>
-                {quantity} x ${price}</span>
+                {quantity} x €{price}</span>
         </div>
     </div>
 );
--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -2,28 +2,28 @@ import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
-    const priceForStripe = price * 100;
-    const publishableKey = 'pk_test_GIwCrWshJJc850lk4joySPTi00t2DVV83z';
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_GIwCrWshJJc850lk4joySPTi00t2DVV83z';
 
-    const onToken = token => {
-        console.log(token);
-        alert('Payment Successfull');
-    }
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
 
-    return (
-        <StripeCheckout 
-            label='Pay Now'
-            name='King Style Clothing Ltd.'
-            billingAddress
-            shippingAddress
-            image='https://svgshare.com/i/JEC.svg'
-            description={`Your total is €${price}`}
-            amount={priceForStripe}
-            panelLabel='Pay Now'
-            token={onToken}
-            stripeKey={publishableKey}
-        />
-    );
-}
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='King Style Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/JEC.svg'
+      description={`Your total is €${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
 
 export default StripeCheckoutButton;
